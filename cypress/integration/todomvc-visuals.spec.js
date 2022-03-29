421810639a623d3e336626d27916f8f3864c5b44
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2022-03-29 16:13:36 +0100

    Added multiple browser/device support

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -6,10 +6,23 @@ describe('Visual validation', () => {
 
     before(() => todoPage.navigate())
 
+    beforeEach(() => cy.eyesOpen({appName: 'TAU Todomvc', batchName: 'TAU Todomvc Hey!', 
+        browser: [
+            {name: 'chrome', width: 1024, height: 768},
+            {name: 'chrome', width: 800, height: 600},
+            {name: 'firefox', width: 1024, height: 768},
+            {deviceName: 'iPhone X'}
+        ]}))
+    afterEach(() => cy.eyesClose())
+
     it('Should look good', () => {
+        cy.eyesCheckWindow('Empty todo list')
+
         todoPage.addTodo("Clean room")
         todoPage.addTodo("Learn JavaScript")
+        cy.eyesCheckWindow('Two todos')
         
         todoPage.toggleItem(1)
+        cy.eyesCheckWindow('Marked as completed')
     })
 })
