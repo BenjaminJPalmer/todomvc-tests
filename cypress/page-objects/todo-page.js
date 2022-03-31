9662828fb9cd6543eade54d5d492430cb67106b7
Benjamin Palmer <benpalmer@ascensor.co.uk>
2022-03-31 16:23:55 +0100

    Added comments to page objects

--- cypress/page-objects/todo-page.js
+++ cypress/page-objects/todo-page.js
@@ -5,14 +5,17 @@ export class TodoPage {
     }
 
     addTodo(todoText) {
+        // Add a new todo
         cy.get(".new-todo").type(`${todoText}{enter}`)
     }
 
     validateTodoTxt(todoIndex, expectedText) {
+        // Verify the {todoIndex} text is correct
         cy.get(`.todo-list li:nth-child(${todoIndex + 1}) label`).should('have.text', expectedText)
     }
 
     toggleItem(n) {
+        // Toggle a nth todo item
         cy.get(`.todo-list li:nth-child(${n}) .toggle`).click()
     }
 }
